1eee3ec31d04a227242d992735d7bff97110beb4
Awesomecoder26 <isaackhan21@gmail.com>
2021-05-03 17:04:49 +0100

    Created Sample Snippet Database

---
+++ src/SnippetDatabase.js
@@ -0,0 +1,20 @@
+export const snippetDatabase = [
+  {
+    id: 0,
+    title: "Title1",
+    description: "des1",
+    snippet: "Snippet1",
+  },
+  {
+    id: 1,
+    title: "Title2",
+    description: "des2",
+    snippet: "Snippet2",
+  },
+  {
+    id: 2,
+    title: "Title3",
+    description: "des3",
+    snippet: "Snippet3",
+  },
+];
