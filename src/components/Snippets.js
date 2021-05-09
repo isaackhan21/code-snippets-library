7833e8bdc834a976d631405ea0e9dc4a8b03a6a9
Awesomecoder26 <isaackhan21@gmail.com>
2021-05-09 09:56:17 +0100

    Added Search filter to snippets sidebar

--- src/components/Snippets.js
+++ src/components/Snippets.js
@@ -1,16 +1,35 @@
 import SnippetPreview from "./SnippetPreview";
+import { useState } from "react";
 
 const Snippets = ({ snippets, onDelete }) => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <>
-      {snippets.map((snippet) => (
-        <SnippetPreview
-          key={snippet.id}
-          snippet={snippet}
-          onDelete={onDelete}
-        />
-      ))}
-    </>
+    <div className="snippets">
+      <input
+        type="text"
+        placeholder="Search Snippet..."
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      {snippets
+        .filter((snippet) => {
+          if (searchTerm == "") {
+            return snippet;
+          } else if (
+            snippet.title.toLowerCase().includes(searchTerm.toLowerCase())
+          ) {
+            return snippet;
+          }
+        })
+        .map((snippet) => (
+          <SnippetPreview
+            key={snippet.id}
+            snippet={snippet}
+            onDelete={onDelete}
+          />
+        ))}
+    </div>
   );
 };
 
